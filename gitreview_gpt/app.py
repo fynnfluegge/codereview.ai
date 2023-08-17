8d827f907647ce2b01b120712f1622c1a5e623e3
fynnfluegge <fynnfluegge@gmx.de>
2023-08-17 13:16:35 +0200

    Refactor code and add commit message prompt for commit action
    - Refactored the code to improve readability and maintainability.
    - Added a commit message prompt for the commit action, allowing the user to enter a custom commit message.
    - Added a check for the `OPENAI_API_KEY` environment variable and exit if it is not found.
    - Added a check for the `args.action` value and exit if it is not provided.
    - Added a check for the `diff_text` variable and exit if it is empty.
    - Added a `readonly` argument to support a readonly mode for reviewing changes without applying them to the files.
    - Updated the `run()` function to handle the new changes and perform the appropriate actions based on the provided arguments.
    
    Note: The commit message prompt is only shown for the commit action, and the changes will only be committed if the user confirms with "y".
    
    Please review and merge these changes.

--- gitreview_gpt/app.py
+++ gitreview_gpt/app.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import argparse
+import sys
 import gitreview_gpt.prompt as prompt
 import gitreview_gpt.formatter as formatter
 import gitreview_gpt.utils as utils
@@ -8,10 +9,12 @@ import gitreview_gpt.request as request
 import gitreview_gpt.reviewer as reviewer
 
 
-# Return the code changes as a git diff
 def get_git_diff(branch):
+    """
+    Return the code changes as a git diff
+    """
     if not branch:
-        command = ["git", "diff"]
+        command = ["git", "diff", "HEAD"]
     else:
         command = ["git", "diff", branch, "--cached"]
 
@@ -20,8 +23,10 @@ def get_git_diff(branch):
     return git_diff.stdout
 
 
-# Process response json and draw output to console
 def print_review_from_response_json(feedback_json):
+    """
+    Process response json and draw output to console
+    """
     print("✨ Review Result ✨")
     for file in feedback_json:
         if feedback_json[file]:
@@ -33,6 +38,9 @@ def print_review_from_response_json(feedback_json):
 def apply_review_to_file(
     api_key, review_json, file_paths, code_change_chunks, guided, gpt_model
 ):
+    """
+    Apply review to file
+    """
     if review_json is not None:
         print_review_from_response_json(review_json)
         for index, file in enumerate(review_json):
@@ -52,13 +60,16 @@ def apply_review_to_file(
                         )
             else:
                 print(
-                    f"⚠️  There are unstaged changes in {utils.get_bold_text(file)}. "
+                    f"⚠️ There are unstaged changes in {utils.get_bold_text(file)}. "
                     + "Please commit or stage them. "
                     + "Applying review changes skipped for now."
                 )
 
 
 def run():
+    """
+    Main function to run the script
+    """
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -76,34 +87,37 @@ def run():
         + "Ask for confirmation before reviewing and applying changes for each file.",
     )
     parser.add_argument(
+        "readonly",
+        action="store_true",
+        help="Readonly mode. Review changes without applying them to the files.",
+    )
+    parser.add_argument(
         "--gpt4", action="store_true", help="Use GPT-4 (default: GPT-3.5)"
     )
 
-    # Parse the command-line arguments
     args = parser.parse_args()
 
     api_key = os.environ.get("OPENAI_API_KEY")
 
     if not api_key:
         print("OPENAI_API_KEY not found.")
-        exit()
+        sys.exit()
 
     if not args.action:
-        exit()
+        sys.exit()
 
     diff_text = None
 
     if args.action == "review":
         diff_text = get_git_diff(args.branch)
-
-    if args.action == "commit":
+    elif args.action == "commit":
         diff_text = subprocess.run(
             ["git", "diff", "--cached"], capture_output=True, text=True
         ).stdout
 
     if not diff_text:
         print("No git changes.")
-        exit()
+        sys.exit()
 
     (
         formatted_diff,
@@ -115,11 +129,9 @@ def run():
     git_diff_token_count = utils.count_tokens(formatted_diff)
 
     if args.action == "review":
-        gpt_model = args.gpt4 and prompt.GptModel.GPT_4 or prompt.GptModel.GPT_35
+        gpt_model = prompt.GptModel.GPT_4 if args.gpt4 else prompt.GptModel.GPT_35
         review_files_separately = git_diff_token_count > gpt_model.value - 2048
 
-        # Check if the token count exceeds the limit of 3072 (1024 tokens for response)
-        # if yes, review the files separately
         if review_files_separately:
             if args.guided:
                 print(
@@ -127,7 +139,6 @@ def run():
                     + "The Review will be splitted into multiple requests."
                 )
 
-            # iterate over the file chunks in the git diff
             for key, value in diff_file_chunks.items():
                 review_file = False
                 if args.guided:
@@ -137,34 +148,34 @@ def run():
                     file_tokens = utils.count_tokens(value)
                     if file_tokens > gpt_model.value - 2048:
                         print(
-                            "TODO: token count exceeds 3072 for a file. "
+                            "The token count exceeds the limit for a file. "
                             + "Split file changes into chunk of changes."
                         )
                         continue
                     review_json = reviewer.request_review(api_key, value, gpt_model)
-                    apply_review_to_file(
-                        api_key,
-                        review_json,
-                        {key: file_paths[key]},
-                        [code_change_chunks[key]],
-                        args.guided,
-                        gpt_model,
-                    )
+                    if not args.readonly:
+                        apply_review_to_file(
+                            api_key,
+                            review_json,
+                            {key: file_paths[key]},
+                            [code_change_chunks[key]],
+                            args.guided,
+                            gpt_model,
+                        )
 
-        # Review the changes in one request
         else:
             review_json = reviewer.request_review(api_key, formatted_diff, gpt_model)
-            apply_review_to_file(
-                api_key,
-                review_json,
-                file_paths,
-                code_change_chunks,
-                args.guided,
-                gpt_model,
-            )
+            if not args.readonly:
+                apply_review_to_file(
+                    api_key,
+                    review_json,
+                    file_paths,
+                    code_change_chunks,
+                    args.guided,
+                    gpt_model,
+                )
 
-    # Create a commit message using OpenAI API
-    if args.action == "commit":
+    elif args.action == "commit":
         payload = prompt.get_commit_message_prompt(formatted_diff)
         commit_message = request.send_request(
             api_key, payload, "Creating commit message..."
@@ -175,6 +186,5 @@ def run():
         user_input = input().lower()
 
         if user_input == "y":
-            # Commit the changes
             commit_command = ["git", "commit", "-m", commit_message]
             subprocess.run(commit_command, capture_output=True, text=True)
